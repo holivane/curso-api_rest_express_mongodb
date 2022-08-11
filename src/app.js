a827b8cabed67a561dab9273de881bfca5ef9542
holivane <holivane@gmail.com>
2022-08-10 21:38:23 -0300

    aplicado CRUD

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json())
+
 const livros = [
     {id:1, "titulo": "JavaScript"},
     {id:2, "titulo": "Código Limpo"},
@@ -12,7 +14,34 @@ app.get('/',(req, res) => {
 })
 
 app.get('/livros',(req, res) => {
-    res.status(200).json(livros)
+    res.status(200).json(livros);
 })
+
+app.get('/livros/:id',(req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
+})
+
+app.post('/livros',(req, res) => {
+    livros.push(req.body);
+    res.status(201).send('Livro cadastrado com sucesso')
+})
+
+app.put('/livros/:id',(req, res) => {
+    let {id} = req.params;
+    let index = buscaLivro(id);
+    livros[index].titulo = req.body.titulo;
+    res.status(201).json(livros);
+})
+
+app.delete('/livros/:id',(req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.send(`Livro "${livros[index].titulo}" apagado com sucesso`);
+})
+
+function buscaLivro(id){
+    return livros.findIndex(livro => livro.id == id)
+}
 
 export default app
